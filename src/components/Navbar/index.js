136bd0c5a6f68bbc4993fa3347dcf3150c29c9c0
Josh Zappone <zappone.josh@gmail.com>
2020-10-31 20:52:34 -0700

    moves logo for deploy

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./style.css";
+import logo from "../../logo-1.png";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
       <p className="navbar-brand site-name">
-        <img className="logo" src="/logo-1.png" alt="logo" />
+        <img className="logo" src={logo} alt="logo" />
         theDirectory
       </p>
     </nav>
